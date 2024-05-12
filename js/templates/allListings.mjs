3d3b5f61835aa88f5b98e3854a39859c3bcbcd3d
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-05-12 10:16:11 +0200

    makes all images and cards the same size

--- js/templates/allListings.mjs
+++ js/templates/allListings.mjs
@@ -2,16 +2,20 @@ import { getHighestBid } from "../utils/highestBid.mjs";
 
 import { countdownTimer } from "../utils/countdown.mjs";
 
+
+
 export function createListingElement(item) {
-  // Create elements
   const listingWrapper = document.createElement("div");
   listingWrapper.classList.add("col-10", "col-md-6", "col-lg-4", "m-auto", "mb-3");
+
   const card = document.createElement("div");
-  card.classList.add("card", "h-100", "m-auto");
+  card.classList.add("card", "h-100", "m-auto", "pb-2");
+
   const img = document.createElement("img");
-  img.classList.add("card-img-top", "h-auto", "m-auto");
+  img.classList.add("card-img-top", "h-auto", "m-auto", "listing-img");
   img.src = item.media && item.media.length > 0 ? item.media[0].url : "../../images/noImage.jpg";
   img.alt = "listing image";
+
   const cardOverlay = document.createElement("div");
   cardOverlay.classList.add("card-img-overlay", "d-flex", "flex-column", "text-center");
   const overlayInner = document.createElement("div");
@@ -23,26 +27,28 @@ export function createListingElement(item) {
   timerSpan.classList.add("headline-text");
   timerSpan.setAttribute("id", "timer");
   timerSpan.textContent = countdownTimer(item);
+
   const cardBody = document.createElement("div");
   cardBody.classList.add("card-body", "m-auto");
   const title = document.createElement("h3");
   title.classList.add("card-title", "text-center");
   title.textContent = item.title;
-  const list = document.createElement("ul");
-  list.classList.add("list-unstyled");
-  const bids = document.createElement("li");
+
+  const infoContainer = document.createElement("p");
+  infoContainer.classList.add("div");
+  const bids = document.createElement("p");
   bids.textContent = `Bids: ${item._count.bids}`;
-  const highestBid = document.createElement("li");
+  const highestBid = document.createElement("p");
   highestBid.textContent = `Highest Bid: ${getHighestBid(item.bids)}`;
+
   const buttonDiv = document.createElement("div");
   buttonDiv.classList.add("text-center", "mt-2");
   const viewButton = document.createElement("button");
   viewButton.classList.add("btn", "btn-primary", "btn-sm", "py-0", "my-2");
-  viewButton.setAttribute("data-bs-toggle", "modal");
-  viewButton.setAttribute("data-bs-target", "#bidModal");
+  
   viewButton.textContent = "View Listing";
 
-  // Append elements
+ 
   cardOverlay.appendChild(overlayInner);
   overlayInner.appendChild(timerTitle);
   overlayInner.appendChild(timerSpan);
@@ -50,9 +56,9 @@ export function createListingElement(item) {
   card.appendChild(cardOverlay);
   card.appendChild(cardBody);
   cardBody.appendChild(title);
-  cardBody.appendChild(list);
-  list.appendChild(bids);
-  list.appendChild(highestBid);
+  cardBody.appendChild(infoContainer);
+  infoContainer.appendChild(bids);
+  infoContainer.appendChild(highestBid);
   cardBody.appendChild(buttonDiv);
   buttonDiv.appendChild(viewButton);
   listingWrapper.appendChild(card);
@@ -60,7 +66,7 @@ export function createListingElement(item) {
 }
 
 export function renderAllListingTemplates(listingDataList, parent) {
-  parent.innerHTML = ""; // Clear the existing content
+  parent.innerHTML = ""; 
   listingDataList.forEach((listingData) => {
     const listingElement = createListingElement(listingData);
     parent.appendChild(listingElement);
@@ -73,7 +79,7 @@ export async function renderSingleListingTemplate(listingData, parent) {
     if (listingElement) {
       const existingListingElement = parent.querySelector(`[data-listing-id="${listingData.id}"]`);
       if (existingListingElement) {
-        // If listing element already exists, replace its content with the new one
+       
         existingListingElement.replaceWith(listingElement);
       } else {
         // Otherwise, append the new listing element

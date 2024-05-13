5656ee2020961ae467c1ab5fc37eb71528e974d5
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-05-13 21:37:21 +0200

    updates index and router so login works on all pages

--- js/index.mjs
+++ js/index.mjs
@@ -1,5 +1,11 @@
+import * as listeners from "./handlers/index.mjs";
+import * as utils from "./utils/index.mjs";
+import * as auth from "./api/index.mjs";
 import { router } from "./router.mjs";
 
 document.addEventListener("DOMContentLoaded", function () {
   router();
+  utils.updateProfileNav();
+  listeners.setLoginFormListener();
+  auth.handleLogout();
 });
--- js/router.mjs
+++ js/router.mjs
@@ -1,26 +1,14 @@
-// router.mjs
 import * as listeners from "./handlers/index.mjs";
-import * as utils from "./utils/index.mjs";
-import * as auth from "./api/index.mjs";
 
 export function router() {
   const path = location.pathname;
 
   switch (path) {
-
-
     case "/listings/allListings/":
-      listners.displayAllListings();
+      listeners.displayAllListings();
       return;
     case "/listings/singleListing/":
-      listners.displaySingleListing();
-
+      listeners.displaySingleListing();
       return;
-      
-         default:
-      utils.updateProfileNav();
-
-      listeners.setLoginFormListener();
-      auth.handleLogout();
   }
 }

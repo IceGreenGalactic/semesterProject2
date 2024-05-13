e2efc605f7703a8413c43910ba1d2f509eb2aaee
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-05-13 21:45:59 +0200

    removes style overflow from modals and moves modals to folder

--- js/handlers/modals/registrationModal.mjs
+++ js/handlers/modals/registrationModal.mjs
@@ -3,26 +3,25 @@
 import { registrationModal } from "../../templates/registrationModal.mjs";
 import { openLoginModal } from "./loginModal.mjs";
 
-
 export const openRegistrationModal = () => {
   const modal = new bootstrap.Modal(registrationModal);
   modal.show();
   document.body.addEventListener("click", backdropClickHandler);
 };
 
-
 export const closeRegistrationModal = () => {
   const modal = new bootstrap.Modal(registrationModal);
   modal.hide();
-  const modalElement = document.getElementById('registrationModal');
-  modalElement.classList.remove('show');
-  modalElement.style.display = 'none'; 
+  const modalElement = document.getElementById("registrationModal");
+  modalElement.classList.remove("show");
+  modalElement.style.display = "none";
 
   // Remove the backdrop element if it exists
-  const backdrop = document.querySelector('.modal-backdrop');
+  const backdrop = document.querySelector(".modal-backdrop");
   if (backdrop) {
     backdrop.parentNode.removeChild(backdrop);
   }
+  document.body.style.overflow = "";
   document.body.removeEventListener("click", backdropClickHandler);
 };
 
@@ -33,7 +32,6 @@ const backdropClickHandler = (event) => {
     closeRegistrationModal();
   }
 };
-
 
 document.addEventListener("DOMContentLoaded", function () {
   const logInLink = document.querySelector("#logInLink");

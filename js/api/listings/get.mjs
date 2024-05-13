2e3e1d099930760ddaa234f663aeb5c73c0279f0
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-05-13 14:28:30 +0200

    changes autFetch to fetch so users that are not loged in kan view items

--- js/api/listings/get.mjs
+++ js/api/listings/get.mjs
@@ -1,15 +1,14 @@
 import { listingsEndpoint } from "../api_constants.mjs";
-import { authFetch } from "../authFetch.mjs";
 
-export async function getListings() {
-    try {
-        const response = await authFetch(listingsEndpoint);
-        if (!response.ok) {
-            throw new Error(`Failed to fetch listings: ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error('Error fetching listings:', error);
-        throw error;
+export async function getAllListings() {
+  try {
+    const response = await fetch(listingsEndpoint);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch listings: ${response.status}`);
     }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching listings:", error);
+    throw error;
+  }
 }

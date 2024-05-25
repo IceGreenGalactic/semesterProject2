73f8af13fed6633bce25e212136983ff5cc90fc8
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-05-25 23:21:47 +0200

    removes logout-link if user not loged in

--- js/utils/auth/isLoggedin.mjs
+++ js/utils/auth/isLoggedin.mjs
@@ -1,8 +1,6 @@
 import { load } from "../../storage/token.mjs";
 import { openLoginModal } from "../../handlers/index.mjs";
 
-// Check if the user is logged in
-
 export function isLoggedIn() {
   const accessToken = load("accessToken");
   return accessToken !== null;
@@ -11,16 +9,19 @@ export function isLoggedIn() {
 export function updateProfileNav() {
   const profileNavLink = document.getElementById("profileNavLink");
   const profileHamburgerLink = document.getElementById("profileHamburgerLink");
+  const logoutLinks = document.querySelectorAll(".logout-link");
 
   if (isLoggedIn()) {
-    // User is logged in
     profileNavLink.innerHTML = '<i class="fas fa-user"></i> Profile';
     profileNavLink.href = "../../../profile/";
 
     profileHamburgerLink.innerHTML = '<i class="fas fa-user"></i> Profile';
     profileHamburgerLink.href = "../../../profile/";
+
+    logoutLinks.forEach((link) => {
+      link.style.display = "block";
+    });
   } else {
-    // User is not logged in
     profileNavLink.innerHTML = '<i class="fas fa-sign-in-alt"></i> LogIn';
     profileNavLink.addEventListener("click", function (event) {
       event.preventDefault();
@@ -31,6 +32,10 @@ export function updateProfileNav() {
     profileHamburgerLink.addEventListener("click", function (event) {
       event.preventDefault();
       openLoginModal();
+    });
+
+    logoutLinks.forEach((link) => {
+      link.style.display = "none";
     });
   }
 }
